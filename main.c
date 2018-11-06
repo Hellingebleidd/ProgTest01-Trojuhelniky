72a67928d3f0f153db9215884d97865fa6f124da
Radovano <losfakiros@gmail.com>
2018-11-07 00:13:30 +0100

    Este neporovnava zhodne trojuholniky

--- main.c
+++ main.c
@@ -1,54 +1,89 @@
 #include <stdio.h>
 #include <math.h>
 
-const int N = 6;
+const int N = 3;
 const int M = 2;
-const int OBVOD = 1;
-const int ROVNAKE = 0;
-const int T1VACSI = 2;
-const int T2VACSI = 3;
 const int NIE_TROJUHOLNIK = 4;
 
 
-int trojuholnikySuRovnake(double v[N][M]) {
-    double dlzka[2][3] = {{0, 0, 0},
-                          {0, 0, 0}};     //vypocitane dlzky stran
-    int p1, p2;             //pozicie pre v
+int rozmerTrojuholnika(double v[N][M], double *dlzka) {
+    int p2;             //pozicie pre v
 
-    for (int k = 0; k < 2; ++k) {   //najprv 1. potom 2. trojuholnik
-        for (int i = 0; i < 3; ++i) {
-            p1 = (3 * k) + i;           //hodnota v[p1][] pre prvu suradnicu
-            if (p1 == 2) { p2 = 0; }
-            else if (p1 == 5) { p2 = 3; }
-            else { p2 = p1 + 1; }
+    for (int i = 0; i < 3; ++i) {
+        if (i == 2) { p2 = 0; } else { p2 = i + 1; }
 
-            //dlzku strany urcim pytagorovou vetou
-            dlzka[k][i] = sqrt(pow((v[p1][0] - v[p2][0]), 2) + pow((v[p1][1] - v[p2][1]), 2));
-            printf("pre p1:%d  p2:%d  je dlzka[%d][%d] == %lf\n", p1, p2, k, i, dlzka[k][i]);
-        }
+        //dlzku strany urcim pytagorovou vetou
+        dlzka[i] = sqrt(pow((v[i][0] - v[p2][0]), 2) + pow((v[i][1] - v[p2][1]), 2));
+//        printf("pre p1:%d  p2:%d  je dlzka[%d] == %lf\n", p1, p2, i, dlzka[i]);
     }
+    if ((dlzka[0] >= (dlzka[1] + dlzka[2])) || (dlzka[1] >= (dlzka[2] + dlzka[0])) ||
+        (dlzka[2] >= (dlzka[0] + dlzka[1])))
+        return NIE_TROJUHOLNIK;
+    return 0;
+}
 
-
-    if((dlzka[0][0]+dlzka[0][1]+dlzka[0][2]) == (dlzka[1][0]+dlzka[1][1]+dlzka[1][2]) )
-        return OBVOD;
-
-    return NIE_TROJUHOLNIK;
-};
+int nacitajSuradnice(double *x, double *y) {
+    int vysledok;
+    vysledok = scanf("%lf %lf", x, y);
+    if (vysledok < 2) {
+        printf("Nespravny vstup.\n");
+        return -1;
+    }
+    return 2;
+}
 
 int main() {
-    char odpovede[5][] ={"Trojuhelniky jsou shodne.", "Trojuhelniky nejsou shodne, ale maji stejny obvod.", "", "", ""};
-    double vrcholy[6][2] = {{0,   0},
-                            {5,   0},
-                            {2.5, 3},
-                            {4,   -1},
-                            {7,   1.5},
-                            {4,   4}};   //vrcholy trojuholnika
-
-    printf("Trojuhelnik #1:\nBod A:\n");
-    printf("%s\n", odpovede[trojuholnikySuRovnake(vrcholy)]);
+    double dlzka1[3] = {0, 0, 0}, dlzka2[3] = {0, 0, 0};
+    double vrcholy1[3][2], vrcholy2[3][2];   //vrcholy trojuholnika};
+    double o1, o2;      //obvod
 
 
+    printf("Trojuhelnik #1:\n");
+    printf("Bod A:\n");
+    if (nacitajSuradnice(&vrcholy1[0][0], &vrcholy1[0][1]) < 2)
+        return -1;
+    printf("Bod B:\n");
+    if (nacitajSuradnice(&vrcholy1[1][0], &vrcholy1[1][1]) < 2)
+        return -1;
+    printf("Bod C:\n");
+    if (nacitajSuradnice(&vrcholy1[2][0], &vrcholy1[2][1]) < 2)
+        return -1;
 
+    if (rozmerTrojuholnika(vrcholy1, dlzka1) == NIE_TROJUHOLNIK) {
+        printf("Body netvori trojuhelnik.\n");
+        return -1;
+    }
+
+    printf("Trojuhelnik #2:\n");
+    printf("Bod A:\n");
+    if (nacitajSuradnice(&vrcholy2[0][0], &vrcholy2[0][1]) < 2)
+        return -1;
+    printf("Bod B:\n");
+    if (nacitajSuradnice(&vrcholy2[1][0], &vrcholy2[1][1]) < 2)
+        return -1;
+    printf("Bod C:\n");
+    if (nacitajSuradnice(&vrcholy2[2][0], &vrcholy2[2][1]) < 2)
+        return -1;
+
+    if (rozmerTrojuholnika(vrcholy2, dlzka2) == NIE_TROJUHOLNIK) {
+        printf("Body netvori trojuhelnik.\n");
+        return -1;
+    }
+
+    if (1==2){
+        printf("Trojuhelniky jsou shodne.\n");
+        return 0;
+    }
+    o1 = dlzka1[0] + dlzka1[1] + dlzka1[2];
+    o2 = dlzka2[0] + dlzka2[1] + dlzka2[2];
+
+    if (o1 == o2)
+        printf("Trojuhelniky nejsou shodne, ale maji stejny obvod.\n");
+
+    if (o1 > o2)
+        printf("Trojuhelnik #1 ma vetsi obvod.\n");
+    else
+        printf("Trojuhelnik #2 ma vetsi obvod.\n");
 
     return 0;
 }
