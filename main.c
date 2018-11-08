57f3d2e622ab90b9cd0d890aa16793a55fccca54
Fakiros <losfakiros@gmail.com>
2018-11-08 19:12:11 +0100

    Zjednodusena verzia, len main, bez funkcii. Pridany test runner - run_tests.cmd, ktory spusti app s testovacimi vstupmi a zobrazi vysledky. Pridany test runner - run_test_and_compate.cmd, ktory navyse porovnava vysledky s ocakavanymi vystupmi. Do .gitignore pridane vystupne subory z testovania.

--- main.c
+++ main.c
@@ -1,110 +1,71 @@
 #include <stdio.h>
 #include <math.h>
 
-const int N = 3;
-const int M = 2;
-const int NIE_TROJUHOLNIK = 4;
-const int SU_ZHODNE = 13;
-
-
-int rozmerTrojuholnika(double v[N][M], double *dlzka) {
-    int p2;             //pozicie pre v
-
-    for (int i = 0; i < 3; ++i) {
-        if (i == 2) { p2 = 0; } else { p2 = i + 1; }
-        //dlzku strany urcim pytagorovou vetou
-        dlzka[i] = sqrt(pow((v[i][0] - v[p2][0]), 2) + pow((v[i][1] - v[p2][1]), 2));
-        dlzka[i] = (round(10000*(dlzka[i])))/10000;
-    }
-    if ((dlzka[0] >= (dlzka[1] + dlzka[2])) || (dlzka[1] >= (dlzka[2] + dlzka[0])) ||
-        (dlzka[2] >= (dlzka[0] + dlzka[1])))
-        return NIE_TROJUHOLNIK;
-    return 0;
-}
-
-int nacitajSuradnice(double *x, double *y) {
-    //pokusi sa nacitat hodnoty. Ak sa nepodari nacitat obe, tak vrati -1
-    int vysledok;
-    vysledok = scanf("%lf %lf", x, y);
-    if (vysledok < 2) {
-        printf("Nespravny vstup.\n");
-        return -1;
-    }
-    return 2;
-}
-
-void triedDlzkyZostupne(double *d) {
-    //jednoduche triedenie 3 prvkov
+int main() {
+    double dlzka[2][3];
+    double vrcholy[3][2];
+    double o1, o2;                          //obvod
+    int i;  // do cyklov
     double tmp;
-    for (int i = 0; i < 2; ++i) {
-        for (int j = i + 1; j < 3; ++j) {
-            if (d[i] < d[j]) {
-                tmp = d[i];
-                d[i] = d[j];
-                d[j] = tmp;
+
+    for (i = 0; i < 2; ++i) {
+        printf("Trojuhelnik #%d:\n", i + 1);
+        printf("Bod A:\n");
+        if (scanf("%lf %lf", &vrcholy[0][0], &vrcholy[0][1]) < 2) {
+            printf("Nespravny vstup.\n");
+            return -1;}
+        printf("Bod B:\n");
+        if (scanf("%lf %lf", &vrcholy[1][0], &vrcholy[1][1]) < 2) {
+            printf("Nespravny vstup.\n");
+            return -1;
+        }
+        //mozem vypocitat 1. dlzku
+        dlzka[i][0] = (round(10000 * (sqrt(pow((vrcholy[0][0] - vrcholy[1][0]), 2) + pow((vrcholy[0][1] - vrcholy[1][1]), 2))))) / 10000;
+
+        printf("Bod C:\n");
+        if (scanf("%lf %lf", &vrcholy[2][0], &vrcholy[2][1]) < 2) {
+            printf("Nespravny vstup.\n");
+            return -1;
+        }
+        //mozem vypocitat dalsie 2 dlzky a zaroven usporiadat
+        if ((dlzka[i][1] = (round(10000 * (sqrt(pow((vrcholy[1][0] - vrcholy[2][0]), 2) + pow((vrcholy[1][1] - vrcholy[2][1]), 2))))) / 10000)> dlzka[i][0]){
+            //dat na zaciatok pola - pozicia 0
+            tmp = dlzka[i][1];
+            dlzka[i][1] = dlzka[i][0];
+            dlzka[i][0] = tmp;
+        }
+
+        if ((dlzka[i][2] = (round(10000 * (sqrt(pow((vrcholy[2][0] - vrcholy[0][0]), 2) + pow((vrcholy[2][1] - vrcholy[0][1]), 2))))) / 10000)> dlzka[i][1]){
+            //posunut [1] na koniec na [2] a potom este porovnat s navacsou [0]
+            tmp = dlzka[i][2];
+            dlzka[i][2] = dlzka[i][1];
+            dlzka[i][1] = tmp;
+
+            if (tmp > dlzka[i][0]){
+                //dat na zaciatok pola - pozicia 0
+                dlzka[i][1] = dlzka[i][0];
+                dlzka[i][0] = tmp;
             }
         }
-    }
-}
-
-int suTrojuholnikyZhodne(double *dlzka1, double *dlzka2) {
-    //porovnam utriedene dlzky trojuholnikov
-
-    triedDlzkyZostupne(dlzka1);
-    triedDlzkyZostupne(dlzka2);
-
-    if ((dlzka1[0] == dlzka2[0]) && (dlzka1[1] == dlzka2[1]) && (dlzka1[2] == dlzka2[2]))
-        return SU_ZHODNE;
-
-    return 0;
-}
-
-int main() {
-    double dlzka1[3] = {0, 0, 0}, dlzka2[3] = {0, 0, 0};
-    double vrcholy1[3][2], vrcholy2[3][2];  //vrcholy trojuholnika};
-    double o1, o2;                          //obvod
 
 
-    printf("Trojuhelnik #1:\n");
-    printf("Bod A:\n");
-    if (nacitajSuradnice(&vrcholy1[0][0], &vrcholy1[0][1]) < 2)
-        return -1;
-    printf("Bod B:\n");
-    if (nacitajSuradnice(&vrcholy1[1][0], &vrcholy1[1][1]) < 2)
-        return -1;
-    printf("Bod C:\n");
-    if (nacitajSuradnice(&vrcholy1[2][0], &vrcholy1[2][1]) < 2)
-        return -1;
-
-    if (rozmerTrojuholnika(vrcholy1, dlzka1) == NIE_TROJUHOLNIK) {
-        printf("Body netvori trojuhelnik.\n");
-        return -1;
+        //kedze prva dlzka ( dlzka[i][0] ) je najvacsia, staci skontrolovat len ju
+        if (dlzka[i][0] >= (dlzka[i][1] + dlzka[i][2])){
+            printf("Body netvori trojuhelnik.\n");
+            return -1;
+        }
     }
 
-    printf("Trojuhelnik #2:\n");
-    printf("Bod A:\n");
-    if (nacitajSuradnice(&vrcholy2[0][0], &vrcholy2[0][1]) < 2)
-        return -1;
-    printf("Bod B:\n");
-    if (nacitajSuradnice(&vrcholy2[1][0], &vrcholy2[1][1]) < 2)
-        return -1;
-    printf("Bod C:\n");
-    if (nacitajSuradnice(&vrcholy2[2][0], &vrcholy2[2][1]) < 2)
-        return -1;
-
-    if (rozmerTrojuholnika(vrcholy2, dlzka2) == NIE_TROJUHOLNIK) {
-        printf("Body netvori trojuhelnik.\n");
-        return -1;
-    }
-
-    if (suTrojuholnikyZhodne(dlzka1, dlzka2) == SU_ZHODNE) {
+    //zhodne su, ak maju rovnake dlzky (utriedene podla velkosti)
+    if ((dlzka[0][0] == dlzka[1][0]) && (dlzka[0][1] == dlzka[1][1]) && (dlzka[0][2] == dlzka[1][2])){
+        //trojuhelniky su zhodne
         printf("Trojuhelniky jsou shodne.\n");
         return 0;
     }
 
     //zaokruhlenie kvoli nepresnostiam 1.99999999999 a 2.00000
-    o1 = (round(1000*(dlzka1[0] + dlzka1[1] + dlzka1[2])))/1000;
-    o2 = (round(1000*(dlzka2[0] + dlzka2[1] + dlzka2[2])))/1000;
+    o1 = dlzka[0][0] + dlzka[0][1] + dlzka[0][2];
+    o2 = dlzka[1][0] + dlzka[1][1] + dlzka[1][2];
 
 //    printf("o1: %lf  o2: %lf\n", o1, o2);
 
---
+++ testy/run_tests_and_compare.cmd
@@ -0,0 +1,21 @@
+@ set testPrg= ..\cmake-build-debug\Trojuhelniky1.exe
+@ echo testovany program = %testPrg%
+
+@ %testPrg% < 0000_in.txt > 0000_out_zz.txt
+@ %testPrg% < 0001_in.txt > 0001_out_zz.txt
+@ %testPrg% < 0002_in.txt > 0002_out_zz.txt
+@ %testPrg% < 0003_in.txt > 0003_out_zz.txt
+@ %testPrg% < 0003_in.txt > 0003_out_zz.txt
+@ %testPrg% < 0004_in.txt > 0004_out_zz.txt
+@ %testPrg% < 0005_in.txt > 0005_out_zz.txt
+@ %testPrg% < 0006_in.txt > 0006_out_zz.txt
+@ %testPrg% < 0007_in.txt > 0007_out_zz.txt
+
+@ fc /C /L 0000_out_zz.txt 0000_out_win.txt
+@ fc /C /L 0001_out_zz.txt 0001_out_win.txt
+@ fc /C /L 0002_out_zz.txt 0002_out_win.txt
+@ fc /C /L 0003_out_zz.txt 0003_out_win.txt
+@ fc /C /L 0004_out_zz.txt 0004_out_win.txt
+@ fc /C /L 0005_out_zz.txt 0005_out_win.txt
+@ fc /C /L 0006_out_zz.txt 0006_out_win.txt
+@ fc /C /L 0007_out_zz.txt 0007_out_win.txt
