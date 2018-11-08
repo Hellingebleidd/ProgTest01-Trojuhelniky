3286bb03c9daf77e59ab68bd4107449ceb7e7f13
Fakiros <losfakiros@gmail.com>
2018-11-08 21:03:57 +0100

    Zjednodusena verzia, len main, bez funkcii. Pridany test runner - run_tests.cmd, ktory spusti app s testovacimi vstupmi a zobrazi vysledky. Pridany test runner - run_test_and_compate.cmd, ktory navyse porovnava vysledky s ocakavanymi vystupmi. Do .gitignore pridane vystupne subory z testovania.

--- testy/run_tests_and_compare.cmd
+++ testy/run_tests_and_compare.cmd
@@ -11,11 +11,11 @@
 @ %testPrg% < 0006_in.txt > 0006_out_zz.txt
 @ %testPrg% < 0007_in.txt > 0007_out_zz.txt
 
-@ fc /C /L 0000_out_zz.txt 0000_out_win.txt
-@ fc /C /L 0001_out_zz.txt 0001_out_win.txt
-@ fc /C /L 0002_out_zz.txt 0002_out_win.txt
-@ fc /C /L 0003_out_zz.txt 0003_out_win.txt
-@ fc /C /L 0004_out_zz.txt 0004_out_win.txt
-@ fc /C /L 0005_out_zz.txt 0005_out_win.txt
-@ fc /C /L 0006_out_zz.txt 0006_out_win.txt
-@ fc /C /L 0007_out_zz.txt 0007_out_win.txt
+@ fc 0000_out_zz.txt 0000_out_win.txt
+@ fc 0001_out_zz.txt 0001_out_win.txt
+@ fc 0002_out_zz.txt 0002_out_win.txt
+@ fc 0003_out_zz.txt 0003_out_win.txt
+@ fc 0004_out_zz.txt 0004_out_win.txt
+@ fc 0005_out_zz.txt 0005_out_win.txt
+@ fc 0006_out_zz.txt 0006_out_win.txt
+@ fc 0007_out_zz.txt 0007_out_win.txt
