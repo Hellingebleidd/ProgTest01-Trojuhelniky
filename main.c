61bc9a3b2075ea9b5f300c7fdd45e92935ebac28
Fakiros <losfakiros@gmail.com>
2018-11-11 14:29:06 +0100

    Odstranena kontrola newline na konci riadku. Upravene zaokruhlovanie. Pouziva sa len pri urcenie ci je to trojuholnik (sucet 2 stran je vacsi ako 3. strana) a pri urceni obvodu. Zaokruhluje sa na 10000

--- main.c
+++ main.c
@@ -1,48 +1,62 @@
 #include <stdio.h>
 #include <math.h>
 
+
+double dlzkaStrany(double ax, double ay, double bx, double by) {
+/*    double dlzka;
+    const int PRESNOST = 1;
+
+    dlzka = sqrt(pow(ax - bx, 2) + pow(ay - by, 2));
+    if (PRESNOST == 1)
+        return dlzka;
+    else
+        return (round(PRESNOST * dlzka)) / PRESNOST;*/
+    return sqrt(pow(ax - bx, 2) + pow(ay - by, 2));
+}
+
 int main() {
+    const int PRESNOST = 10000;
+
     double dlzka[2][3];
     double vrcholy[3][2];
     double o1, o2;                          //obvod
     int i;  // do cyklov
     double tmp;
-    char c;
-    const int Zaokruhlenie=1000;
+//    char c;
 
     for (i = 0; i < 2; ++i) {
         printf("Trojuhelnik #%d:\n", i + 1);
         printf("Bod A:\n");
-        if ((scanf("%lf %lf%c", &vrcholy[0][0], &vrcholy[0][1], &c) != 3) || (c != '\n' && c != ' ')) {
+//        if ((scanf("%lf %lf%c", &vrcholy[0][0], &vrcholy[0][1], &c) != 3) || (c != '\n' && c != ' ')) {
+        if ((scanf("%lf %lf", &vrcholy[0][0], &vrcholy[0][1]) < 2)) {
             printf("Nespravny vstup.\n");
             return -1;
         }
 
         printf("Bod B:\n");
-        if ((scanf("%lf %lf%c", &vrcholy[1][0], &vrcholy[1][1], &c) != 3) || (c != '\n' && c != ' ')) {
+//        if ((scanf("%lf %lf%c", &vrcholy[1][0], &vrcholy[1][1], &c) != 3) || (c != '\n' && c != ' ')) {
+        if ((scanf("%lf %lf", &vrcholy[1][0], &vrcholy[1][1]) < 2)) {
             printf("Nespravny vstup.\n");
             return -1;
         }
         //mozem vypocitat 1. dlzku
-        dlzka[i][0] = (round(Zaokruhlenie * (sqrt(pow((vrcholy[0][0] - vrcholy[1][0]), 2) +
-                                           pow((vrcholy[0][1] - vrcholy[1][1]), 2))))) / Zaokruhlenie;
+        dlzka[i][0] = dlzkaStrany(vrcholy[0][0], vrcholy[0][1], vrcholy[1][0], vrcholy[1][1]);
 
         printf("Bod C:\n");
-        if ((scanf("%lf %lf%c", &vrcholy[2][0], &vrcholy[2][1], &c) != 3) || (c != '\n' && c != ' ')) {
+//        if ((scanf("%lf %lf%c", &vrcholy[2][0], &vrcholy[2][1], &c) != 3) || (c != '\n' && c != ' ')) {
+        if ((scanf("%lf %lf", &vrcholy[2][0], &vrcholy[2][1]) < 2)) {
             printf("Nespravny vstup.\n");
             return -1;
         }
         //mozem vypocitat dalsie 2 dlzky a zaroven usporiadat
-        if ((dlzka[i][1] = (round(Zaokruhlenie * (sqrt(pow((vrcholy[1][0] - vrcholy[2][0]), 2) +
-                                                pow((vrcholy[1][1] - vrcholy[2][1]), 2))))) / Zaokruhlenie) > dlzka[i][0]) {
+        if ((dlzka[i][1] = dlzkaStrany(vrcholy[1][0], vrcholy[1][1], vrcholy[2][0], vrcholy[2][1])) > dlzka[i][0]) {
             //dat na zaciatok pola - pozicia 0
             tmp = dlzka[i][1];
             dlzka[i][1] = dlzka[i][0];
             dlzka[i][0] = tmp;
         }
 
-        if ((dlzka[i][2] = (round(Zaokruhlenie * (sqrt(pow((vrcholy[2][0] - vrcholy[0][0]), 2) +
-                                                pow((vrcholy[2][1] - vrcholy[0][1]), 2))))) / Zaokruhlenie) > dlzka[i][1]) {
+        if ((dlzka[i][2] = dlzkaStrany(vrcholy[2][0], vrcholy[2][1], vrcholy[0][0], vrcholy[0][1])) > dlzka[i][1]) {
             //posunut [1] na koniec na [2] a potom este porovnat s navacsou [0]
             tmp = dlzka[i][2];
             dlzka[i][2] = dlzka[i][1];
@@ -55,9 +69,11 @@ int main() {
             }
         }
 
-//printf("dlzky: %lf %lf %lf\n", dlzka[i][0], dlzka[i][1], dlzka[i][2]);
+//        printf("dlzky: %lf %lf %lf  obvod: %lf ", dlzka[i][0], dlzka[i][1], dlzka[i][2], dlzka[i][0]+dlzka[i][1]+dlzka[i][2]);
+//        printf("  0-(1+2)= %lf\n", dlzka[i][0] - (dlzka[i][1]+dlzka[i][2]));
         //kedze prva dlzka ( dlzka[i][0] ) je najvacsia, staci skontrolovat len ju
-        if (dlzka[i][0] >= (dlzka[i][1] + dlzka[i][2])) {
+//        if (dlzka[i][0] >= (dlzka[i][1] + dlzka[i][2])) {
+        if ((round(PRESNOST * (dlzka[i][0] - (dlzka[i][1] + dlzka[i][2])))) / PRESNOST == 0) {
             printf("Body netvori trojuhelnik.\n");
             return -1;
         }
@@ -70,8 +86,8 @@ int main() {
         return 0;
     }
 
-    o1 = dlzka[0][0] + dlzka[0][1] + dlzka[0][2];
-    o2 = dlzka[1][0] + dlzka[1][1] + dlzka[1][2];
+    o1 = (round(PRESNOST * (dlzka[0][0] + dlzka[0][1] + dlzka[0][2]))) / PRESNOST;
+    o2 = (round(PRESNOST * (dlzka[1][0] + dlzka[1][1] + dlzka[1][2]))) / PRESNOST;
 
 
     if (o1 < o2)
--- tests/run_tests_and_compare.cmd
+++ tests/run_tests_and_compare.cmd
@@ -5,7 +5,6 @@
 @ %testPrg% < 0001_in.txt > 0001_out_zz.txt
 @ %testPrg% < 0002_in.txt > 0002_out_zz.txt
 @ %testPrg% < 0003_in.txt > 0003_out_zz.txt
-@ %testPrg% < 0003_in.txt > 0003_out_zz.txt
 @ %testPrg% < 0004_in.txt > 0004_out_zz.txt
 @ %testPrg% < 0005_in.txt > 0005_out_zz.txt
 @ %testPrg% < 0006_in.txt > 0006_out_zz.txt
@@ -20,8 +19,8 @@
 @ fc 0005_out_zz.txt 0005_out_win.txt > null && Echo 0005 ok || fc 0005_out_zz.txt 0005_out_win.txt
 @ fc 0006_out_zz.txt 0006_out_win.txt > null && Echo 0006 ok || fc 0006_out_zz.txt 0006_out_win.txt
 @ fc 0007_out_zz.txt 0007_out_win.txt > null && Echo 0007 ok || fc 0007_out_zz.txt 0007_out_win.txt
-
 @ fc 0008_out_zz.txt 0008_out_win.txt > null && Echo 0008 ok || fc 0008_out_zz.txt 0008_out_win.txt
+
 @ rem fc 0009_out_zz.txt 0009_out_win.txt > null && Echo 0009 ok || fc 0009_out_zz.txt 0009_out_win.txt
 @ rem fc 0010_out_zz.txt 0010_out_win.txt > null && Echo 0010 ok || fc 0010_out_zz.txt 0010_out_win.txt
 @ rem fc 0011_out_zz.txt 0011_out_win.txt > null && Echo 0011 ok || fc 0011_out_zz.txt 0011_out_win.txt
